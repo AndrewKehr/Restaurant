89255d4c8bad6f6d68ada889bb0f22a45b427cc1
Andrew Kehr <85807215+AndrewKehr@users.noreply.github.com>
2021-11-29 09:31:01 -0600

    Add files via upload

---
+++ back-end/src/db/seeds/01-tables.js
@@ -0,0 +1,9 @@
+const tablesData = require("./01-tables.json");
+
+console.log(tablesData);
+
+exports.seed = function (knex) {
+  return knex
+    .raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
+    .then(() => knex("tables").insert(tablesData));
+};
